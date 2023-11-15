28cb2f61042cddc32705ff1b5228e8be5485cadc
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-15 10:23:53 +0000

    Receiving longitude & latitude

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_possition = (longitude, latitude)
+
+print(iss_possition)
+
+##Track with the link below##
+#https://www.latlong.net/Show-Latitude-Longitude.html
