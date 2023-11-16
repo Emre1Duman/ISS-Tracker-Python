1af133a07fe39afa9d4bcf9b0072b24edcd19dfb
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-16 22:01:17 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-import requests
+import requests, smtplib
 from datetime import datetime
 
-MY_LAT = 51.507351 
-MY_LNG = -0.127758
+my_email = "email to send from"
+password = "app password"
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
+MY_LAT = 51.507351
+MY_LONG = -0.127758
 
-# data = response.json()
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
 
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-
-# iss_possition = (longitude, latitude)
-
-# print(iss_possition)
-
-##Track with the link below##
-#https://www.latlong.net/Show-Latitude-Longitude.html
-
-#Getting sun set/down times
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
 
 parameters = {
     "lat": MY_LAT,
-    "lng": MY_LNG,
-    "formatted": 0
+    "lng": MY_LONG,
+    "formatted": 0,
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-print(sunrise)
-print(sunset)
+time_now = datetime.now()
+print(time_now.hour)
 
-timenow = datetime.now()
-print(timenow.hour)
+
+if iss_latitude >= 46.507351 and iss_latitude <= 56.507351 and time_now.hour >= sunset:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                            msg="Subject:ISS Tracker Alert \n\n Look up now! ")
+        print("Email sent")
+
+
+#run the code every 60 seconds.
+
+
+
